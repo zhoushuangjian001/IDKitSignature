0cacd1a19c383ba6891bfe6904cd8b90f4def891
zhoushuangjian <zhoushuangjian@jinjvdata.com>
2019-09-05 18:58:32 +0800

    实现绘图

--- IDKitSignature/Classes/IDKitSignatureView.swift
+++ IDKitSignature/Classes/IDKitSignatureView.swift
@@ -14,42 +14,57 @@ import Foundation
     @objc var lineWidth:CGFloat = 5.0;
     /// Set line color
     @objc var lineColor:UIColor = .black
+    /// Set view bgcolor
+    @objc var bgViewColor:UIColor = .white
     
     /// Initialize the container
-    fileprivate var pointsArray:Array<Array<NSValue>> = []
-    fileprivate var tempPointsArray:Array<NSValue>?
+    var pointsArray:NSMutableArray = NSMutableArray.init()
+    var tempPointsArray:NSMutableArray?
 
-    // Get view context
-    fileprivate let context = UIGraphicsGetCurrentContext()
-
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = bgViewColor
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     /// View draw method
     override public func draw(_ rect: CGRect) {
-        // Set
-        context!.setLineWidth(lineWidth)
-        context!.setStrokeColor(lineColor.cgColor)
-        // Set drew start point
-        pointsArray.forEach { (valuesArray) in
-            let startPoint = valuesArray.first!.cgPointValue
-            context!.move(to: startPoint)
-            valuesArray.forEach({ (value) in
-                context!.addLine(to: value.cgPointValue)
-            })
+        // Get view context
+        if let context = UIGraphicsGetCurrentContext() {
+            // Set
+            context.setLineWidth(lineWidth)
+            context.setStrokeColor(lineColor.cgColor)
+            // Set drew start point
+            pointsArray.forEach { (valuesArray) in
+                let mValuesArray = NSMutableArray.init(array: valuesArray as! NSMutableArray)
+                let startPoint = (mValuesArray.firstObject as! NSValue).cgPointValue
+                context.move(to: startPoint)
+                // Remove container frist point
+                mValuesArray.remove(mValuesArray.firstObject!)
+                mValuesArray.forEach({ (value) in
+                    context.addLine(to: (value as! NSValue).cgPointValue)
+                })
+            }
+            context.strokePath()
         }
-        context!.strokePath()
     }
 
     /// View touch began method
     override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         // Initialize container
-        tempPointsArray = Array.init()
+        tempPointsArray = NSMutableArray.init()
         // Temporary points are added to the formal container container
-        pointsArray.append(tempPointsArray!)
+        pointsArray.add(tempPointsArray!)
     }
 
     /// View touch ended method
     override public func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         // Remove elements in the container
-        tempPointsArray!.removeAll()
+//        tempPointsArray!.removeAll()
+        print(pointsArray)
     }
 
     /// View touch moved method
@@ -61,7 +76,7 @@ import Foundation
         // Container can contain points can be converted to objects
         let value = NSValue.init(cgPoint: point)
         // Add points to the temporary containers
-        tempPointsArray?.append(value)
+        tempPointsArray!.add(value)
         // Draw View
         self.setNeedsDisplay()
     }
